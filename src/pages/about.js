e34478823d0ab9e9a82cb9064ea52c3fb1cf38ce
Haider Ali Sehar <haideralisehar254@gmail.com>
2026-01-30 23:19:03 +0500

    Add

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+export default function About() {
+  return (
+    <div className="about-container">
+      <div className="system-status">
+        <span className="status-dot" />
+        <span className="status-text">System Operational</span>
+      </div>
+
+      <h1 className="skeleton-text">Haider</h1>
+    </div>
+  );
+}
